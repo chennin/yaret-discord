9b0cb49aa0188cb994503cf421db31e1dc1244d8
Chris Henning <saturos@gmail.com>
2017-03-24 11:32:03 -0400

    say we're playing RIFT

--- sendevents.py
+++ sendevents.py
@@ -32,6 +32,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('Logged in as ' + client.user.name + "#" + str(client.user.discriminator))
+    await client.change_presence(game=discord.Game(name='Rift'))
 
 async def send_new_events():
     global pausetime
